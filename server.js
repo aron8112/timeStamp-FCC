e3cb021050f6e3c97ec594a346c08781a2c8e481
aron8112 <aron8112@gmail.com>
2022-03-07 12:45:04 -0300

    first attemp resolving the challenge

--- server.js
+++ server.js
@@ -24,7 +24,41 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date?', (req, res) => {
+  let date = req.params;
 
+  // VALIDATORS
+  const dateUnix = /\d{13}/;
+  const dateGmt = /^(?:[0-9]{4})-(?:[(1-12)])-(?:[1-31]{2})$/;
+
+  //CONVERTER
+  try {
+    if (date === dateUnix) {
+      //unix a utc
+      const ccDate = new Date(date*1000)
+      //convertir de milisegundos a segundos
+      const cDate = ccDate.getUTCString();
+      res.send({ unix: date, utc: cDate });
+    }
+    if (date === dateGmt) {
+      //utc a unix
+      const uDate = date.parse()/1000;
+      res.send({ unix: uDate, utc: date });
+    }
+    if (date == null || date == undefined) {
+      const unix = new Date.getTime();
+      const gmt = new Date.getUTCString();
+      res.send({ unix: unix, utc: gmt });
+    if (date !== dateUnix && date !== dateGmt) {
+      res.send({ message: "Invalid Date" })
+    }
+    
+    }
+  }
+  catch (err) {
+    res.json(err);
+  }
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
